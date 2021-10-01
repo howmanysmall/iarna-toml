4f99480610500b7d866a9cfc2412fe8c37309ad0
howmanysmall <lhamil29@bruinmail.slcc.edu>
2021-10-01 13:51:45 -0600

    Update stringify.js

--- stringify.js
+++ stringify.js
@@ -187,7 +187,8 @@ function stringifyInline (value, type) {
 
 function stringifyInteger (value) {
   /* eslint-disable security/detect-unsafe-regex */
-  return String(value).replace(/\B(?=(\d{3})+(?!\d))/g, '_')
+//   return String(value).replace(/\B(?=(\d{3})+(?!\d))/g, '_')
+  return String(value);
 }
 
 function stringifyFloat (value) {
